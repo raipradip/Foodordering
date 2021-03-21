b662bf9537334905e91a4b14a0127f758feef99c
Pradip Rai <bayungpradip@gmail.com>
2021-03-21 23:03:50 +0545

    Images Added

--- functions/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	ob_start(); //search it in internet
-	session_start();
-
-	include("functions.php");
-	include("db.php");
-
-?>
--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include('functions/init.php');
-	session_destroy();
-	redirect('index.php');
- ?>
